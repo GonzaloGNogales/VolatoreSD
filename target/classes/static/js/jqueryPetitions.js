4fa6c5bef48ef122c00a8704b2afbec90651189f
GonzaloGNogales <gonzalognbusiness@gmail.com>
2020-05-26 00:09:22 +0200

    Form finished 10000 percent

--- target/classes/static/js/jqueryPetitions.js
+++ target/classes/static/js/jqueryPetitions.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+	$("#datepickerIda").width("115px");
+	$("#datepickerVuelta").width("115px");
+	
+	if ($("input[name=botonTipoVuelo]:checked").val() == "ida") {
+		$("#datepickerVuelta").hide();
+		$("#labelvuelta").hide();
+	} else if ($("input[name=botonTipoVuelo]:checked").val() == "idayvuelta") {
+		$("#datepickerVuelta").show();
+		$("#labelvuelta").show();
+	}
+	
 	$("#datepickerIda").datetimepicker({
 		format : 'DD/MM/YYYY HH:mm',
 		icons : {
@@ -13,7 +24,7 @@ $(document).ready(function() {
 			close : 'fa fa-remove'
 		}
 	}).datetimepicker();
-
+	
 	$("#datepickerVuelta").datetimepicker({
 		format : 'DD/MM/YYYY HH:mm',
 		icons : {
@@ -28,4 +39,14 @@ $(document).ready(function() {
 			close : 'fa fa-remove'
 		}
 	}).datetimepicker();
+	
+	$("input[name=botonTipoVuelo]:radio").click(function() {
+		if ($("input[name=botonTipoVuelo]:checked").val() == "ida") {
+			$("#datepickerVuelta").hide();
+			$("#labelvuelta").hide();
+		} else if ($("input[name=botonTipoVuelo]:checked").val() == "idayvuelta") {
+			$("#datepickerVuelta").show();
+			$("#labelvuelta").show();
+		}
+	});
 });
